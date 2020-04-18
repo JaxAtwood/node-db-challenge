e42304d4e6865dee0780a5893b505f038f50e96f
Jackie Atwood <takalaj@verizon.net>
2020-04-17 22:48:55 -0400

    Endpoints and functions written

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -7,28 +7,39 @@ module.exports = {
     findAllProjects,
     findAllTasks,
     findAllResources,
+    findAllProjectsById,
 }
 
 function addProject(project) {
-    
+    return db("projects")
+    .insert(project)
 }
 
 function addTask(task) {
-
+    return db("tasks")
+    .insert(task)
 }
 
 function addResource(resource) {
-
+    return db("resources")
+    .insert(resource)
 }
 
 function findAllProjects() {
+    return db("projects")
+}
 
+function findAllProjectsById(id) {
+    return db("projects")
+    .where({id}).first()
 }
 
 function findAllTasks() {
-
+    return db("tasks")
+    .join("projects", "tasks.project_id", "projects.id")
+    .select("tasks.description", "tasks.notes", "tasks.completed", "projects.name", "projects.description")
 }
 
 function findAllResources() {
-    
+    return db("resources")
 }
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -5,3 +5,86 @@ const Projects = require("./projects-model.js");
 const router = express.Router();
 
 module.exports = router;
+
+
+router.get("/", (req, res) => {
+    Projects.findAllProjects()
+    .then(projects => {
+        res.status(200).json(projects);
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "Failed to retrieve projects" });
+    })
+})
+
+router.get("/tasks", (req, res) => {
+    Projects.findAllTasks()
+    .then(tasks => {
+        res.status(200).json(tasks);
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "Failed to retrieve tasks" });
+    })
+})
+
+router.get("/resources", (req, res) => {
+    Projects.findAllResources()
+    .then(resources => {
+        res.status(200).json(resources);
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "Failed to retrieve resources" });
+    })
+})
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    Projects.findAllProjectsById(id)
+    .then(project => {
+        if (project) {
+            res.status(200).json(project)
+        } else {
+            res.status(404).json({ errorMessage: "Could not retrieve this project by given id" })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "Failed to retrieve Project" });
+    })
+})
+
+router.post("/projects", (req, res) => {
+    const projectData = req.body;
+
+    Projects.addProject(projectData)
+    .then(project => {
+        res.status(201).json(project);
+    })
+    .catch(err => {
+        res.status(500).json({errorMessage: "Failed to create new project" });
+    })
+})
+
+router.post("/tasks", (req, res) => {
+    const taskData = req.body;
+
+    Projects.addTask(taskData)
+    .then(task => {
+        res.status(201).json(task);
+    })
+    .catch(err => {
+        res.status(500).json({errorMessage: "Failed to create new task" });
+    })
+})
+
+router.post("/resources", (req, res) => {
+    const resourceData = req.body;
+
+    Projects.addResource(resourceData)
+    .then(resource => {
+        res.status(201).json(resource);
+    })
+    .catch(err => {
+        res.status(500).json({errorMessage: "Failed to create new resource" });
+    })
+})
